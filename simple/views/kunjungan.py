93a6e2fb8e6648a61e0a555a614d004e221c4cc3
Feri Setyaji <39894344+ferisetyaji@users.noreply.github.com>
2022-12-21 07:32:11 +0700

    filter kunjungan

--- simple/views/kunjungan.py
+++ simple/views/kunjungan.py
@@ -12,10 +12,13 @@ def index(request):
 	start_date = None
 	end_date = None
 	s_ruang = None
+
+	data = Kunjungan.objects.all()
+
 	if 'cari' in request.POST:
 		start_date = request.POST['dari']
 		end_date = request.POST['ke']
-		
+
 		if start_date != '' and end_date != '':
 			if len(request.POST['ruang']) != 0:
 				s_ruang = request.POST['ruang']
@@ -26,22 +29,22 @@ def index(request):
 			if len(request.POST['ruang']) != 0:
 				s_ruang = request.POST['ruang']
 				data = Kunjungan.objects.filter(id_ruang=s_ruang)
-	else:
-		data = Kunjungan.objects.all()
+
+		
 
 	ruang = Ruang.objects.all()
-	
+
 	return render(request, 'admin/kunjungan.html', {
 		'kunjungan': data,
-		'ruang': ruang,
 		'dari': start_date,
 		'ke': end_date,
+		'ruang': ruang,
 		's_ruang': s_ruang
 		})
 
 def detail(request):
 	_kunjungan = Kunjungan.objects.filter(id = request.POST['id']).values()[0]
-	_pengunjung = Pengunjung.objects.filter(id = _kunjungan['id_pengunjung']).values()[0]	
+	_pengunjung = Pengunjung.objects.filter(id = _kunjungan['id_pengunjung']).values()[0]
 
 	data = {
 		'kunjungan': _kunjungan,
